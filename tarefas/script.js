65515175fac61bb6a17c5a12fce51df1bd90e18d
Giovanna <giovanna.v.nascimento7@aluno.senai.br>
2026-03-12 09:38:45 -0300

    tarefa 1 concluida

---
+++ tarefas/script.js
@@ -0,0 +1,7 @@
+document.getElementById("btn").addEventListener("click", function(){
+
+let nome = document.getElementById("nome").value
+
+document.getElementById("mensagem").textContent = "Olá " + nome
+
+})
